a9d30235b367b8866465d7734c16cebb113e0638
MalLunBar <malinelundgren1991@gmail.com>
2025-08-05 16:07:04 +0200

    add info to AddLoppis

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,14 +1,25 @@
+//finns det ett bättre sätt att gömma label? 
 
-const Input = ({ type, name, value, onChange, placeholder, required }) => {
+
+const Input = ({ type, name, label, value, onChange, required, ariaHidden }) => {
   return (
-    <input
-      type={type}
-      name={name}
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      required={required}
-    />
+    <>
+      <label
+        htmlFor={name}
+        aria-hidden={ariaHidden}
+      >
+        {label}
+      </label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={label}
+        required={required}
+      />
+    </>
   )
 }
 
--- frontend/src/pages/AddLoppis.jsx
+++ frontend/src/pages/AddLoppis.jsx
@@ -1,8 +1,23 @@
+import Input from "../components/Input"
+
 const AddLoppis = () => {
-  return (
+
+    return (
     <section>
       <h2>Lägg till en loppis</h2>
-      
+      {/*här ska man kunna upload images*/}
+
+      <form action="">
+        <Input label='Loppis namn' type='text' />
+        <Input label='Adress' type='text' />
+        <Input label='Öppettider' type='text' />
+        <Input label='Beskrivning' type='text' />
+        <Input label='Telefonnummer' type='text' />
+
+        <Input label='E-post' type='email' />
+        <Input label='Länk till hemsida' type='url' />
+
+      </form>
     </section>
   )
 }

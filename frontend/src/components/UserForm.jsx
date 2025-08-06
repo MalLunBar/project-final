41a2f9aeedeb5cae2ae9a24fa0efca917afa8aef
MalLunBar <malinelundgren1991@gmail.com>
2025-08-06 13:58:24 +0200

    add functionality for switching between login and register

--- frontend/src/components/UserForm.jsx
+++ frontend/src/components/UserForm.jsx
@@ -3,9 +3,14 @@ import { useState } from "react"
 import Input from "./Input"
 import Button from "./Button"
 
-const UserForm = ({ type, onSubmit }) => {
+const UserForm = ({ type, onSubmit, onSwitchForm }) => {
 
-  const title = type === 'login' ? 'Logga in' : `${type === 'register' ? 'Registrera' : ''}`
+  const titles = {
+    login: 'Logga in',
+    register: 'Registrera'
+  }
+
+  const title = titles[type] || 'Formulär'
 
   const [formData, setFormData] = useState({
     email: "",
@@ -25,14 +30,24 @@ const UserForm = ({ type, onSubmit }) => {
     })
   }
 
+  const isLogin = type === 'login'
+  const switchText = isLogin
+    ? 'Har du inget konto?'
+    : 'Har du redan ett konto?'
+
+  const switchButtonText = isLogin
+    ? 'Registrera dig här'
+    : 'Logga in här'
+
   return (
     <article>
       <div>
-        <h2>{title}</h2>
+        <h2 className="font-bold">{title}</h2>
       </div>
 
       <form
-        onSubmit={handleSubmit}>
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-3 my-6 border">
         <Input
           id={`${type}-email`}
           type='email'
@@ -51,7 +66,6 @@ const UserForm = ({ type, onSubmit }) => {
           value={formData.password}
           showLabel={true}
           required={true}
-
         />
 
         <Button
@@ -59,6 +73,19 @@ const UserForm = ({ type, onSubmit }) => {
           type='submit'
           ariaLabel={title}
         />
+
+        <div>
+          <p>
+            {switchText}{' '}
+            <button
+              type="button"
+              onClick={onSwitchForm}
+            >
+              {switchButtonText}
+            </button>
+          </p>
+
+        </div>
       </form>
     </article>
   )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,31 +1,61 @@
+import { useState } from 'react'
 import { MapPin, CirclePlus } from 'lucide-react'
 import Button from '../components/Button'
 import Input from '../components/Input'
 import UserForm from '../components/UserForm'
 
 const Home = () => {
+  //Test för login knapp 
+  const [showPopup, setShowPopup] = useState(false)
+  const [formType, setFormType] = useState('login')
+
+  const handleOpenLogin = () => {
+    console.log('Logga in knapp klickad!')
+    setFormType('login')
+    setShowPopup(true)
+
+  }
 
   //test av onSubmit. Kan tas bort senare
   const onSubmit = (email, password) => {
-    console.log("Inskickade värden:");
-    console.log("Email:", email);
-    console.log("Password:", password);
+    console.log("Inskickade värden:")
+    console.log("Email:", email)
+    console.log("Password:", password)
+    setShowPopup(false)
   }
+
+
 
   return (
     <section>
+
+      <Button
+        text='Logga in'
+        onClick={handleOpenLogin}
+      />
+
+
       <h2>Välkommen!</h2>
       <h3>Hitta en loppis nära dig</h3>
 
-      <UserForm 
-        type='login'
-        onSubmit={onSubmit}  />
-   
+
+
+      {showPopup && (
+        <UserForm
+          type={formType}
+          onSubmit={onSubmit}
+          onSwitchForm={() =>
+            setFormType((prev) => (prev === 'login' ? 'register' : 'login'))
+          }
+        />
+      )}
+
 
       <Input
         label='Sök efter loppisar'
         type='text'
       />
+
 
       <Button text='Lägg till loppis' icon={CirclePlus} />
 

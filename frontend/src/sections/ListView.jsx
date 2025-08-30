3c1bc7997b7ce2986f31703dbf27b17883589039
Mimmi Eriksson <mimmi.aj.eriksson@gmail.com>
2025-08-30 10:33:28 +0200

    added sortin on text on loppis list view

--- frontend/src/sections/ListView.jsx
+++ frontend/src/sections/ListView.jsx
@@ -6,11 +6,15 @@ const ListView = ({ loppisList }) => {
       <h2 className="sr-only">Sökresultat listvy</h2>
       <div className='flex flex-col gap-2 divide-y divide-border py-5 '>
         <div className='flex pb-2 justify-between gap-2 text-sm md:text-base'>
-
-          <p>Antal träffar: {loppisList.length} st</p>
-
-          {/* Sortera på: dropdown med alternativ */}
-          <p>Sortera på: --- </p>
+          <div className='flex flex-col gap-x-1 min-[360px]:flex-row'>
+            <p className='font-medium'>Antal träffar:</p>
+            <p>{loppisList.length} st</p>
+          </div>
+          <div className='flex flex-col items-end gap-x-1 min-[360px]:flex-row'>
+            <p className='font-medium'>Sortera på:</p>
+            {/* TODO: sortering logik och dropdown med alternativ */}
+            <p>Senast tillagda</p>
+          </div>
 
         </div>
 
